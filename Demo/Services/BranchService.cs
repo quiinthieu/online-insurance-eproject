2d50c05d0e35366c6a40d550d7b64451cb99f795
Nguyen Gia Tuan <nguyengiatuan@Nguyens-MacBook-Pro.local>
2021-10-27 23:16:45 +0700

    Service of Agent-Branch

---
+++ Demo/Services/BranchService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Demo.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Services
+{
+    public class BranchService:IBranchService
+    {
+        private DatabaseContext _databaseContext;
+
+        public BranchService(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public Branch Create(Branch branch)
+        {
+            _databaseContext.Branches.Add(branch);
+            int codeSuccess = _databaseContext.SaveChanges();
+            if (codeSuccess != 0)
+                return branch;
+            return null;
+        }
+
+        public dynamic FindAll()
+        {
+            return _databaseContext.Branches.Select(b => new
+            {
+                b.Id,
+                b.Name,
+                b.Phone,
+                b.Street,
+                b.City,
+                b.State,
+                b.ZipCode
+            }).ToList();
+        }
+
+        public dynamic FindById(int id)
+        {
+            return _databaseContext.Branches.AsNoTracking().Select(b => new
+            {
+                b.Id,
+                b.Name,
+                b.Phone,
+                b.Street,
+                b.City,
+                b.State,
+                b.ZipCode
+            }).ToList();
+        }
+
+        public Branch Update(int id, Branch branch)
+        {
+            var branchToUpdate = FindById(id);
+            if (branchToUpdate != null)
+            {
+                branchToUpdate.Name = branch.Name;
+                branchToUpdate.Phone = branch.Phone;
+                branchToUpdate.Gender = branch.Street;
+                branchToUpdate.BranchId = branch.City;
+                branchToUpdate.State = branch.State;
+                branchToUpdate.ZipCode = branch.ZipCode;
+                _databaseContext.Entry(branchToUpdate).State = EntityState.Modified;
+                return branchToUpdate;
+            }
+            return null;
+        }
+    }
+}
---
+++ Demo/Services/IAgentService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Demo.Models;
+
+namespace Demo.Services
+{
+    public interface IAgentService
+    {
+        public dynamic FindAll();
+        public dynamic FindById(int id);
+        public Agent Create(Agent agent);
+        public Agent Update(int id, Agent agent);
+    }
+}
