c56ab024483edd1a23a8706bd7ddb400eb84362c
Nguyen Gia Tuan <nguyengiatuan@Nguyens-MacBook-Pro.local>
2021-10-28 09:58:33 +0700

    Services of Customer and Insurance type

---
+++ Demo/Services/IInsuranceTypeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.Models;
+
+namespace Demo.Services
+{
+     public interface IInsuranceTypeService
+    {
+        public dynamic FindAll();
+
+        public dynamic FindById(int id);
+
+        public dynamic Create(InsuranceType insuranceType);
+
+        public dynamic Update(InsuranceType insuranceType);
+    }
+}
---
+++ Demo/Services/InsuranceTypeService.cs
@@ -0,0 +1,54 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Services
+{
+    public class InsuranceTypeService : IInsuranceTypeService
+    {
+        private DatabaseContext db;
+
+        public InsuranceTypeService(DatabaseContext _db)
+        {
+            db = _db;
+        }
+        public dynamic Create(InsuranceType insuranceType)
+        {
+            db.InsuranceTypes.Add(insuranceType);
+            db.SaveChanges();
+            return insuranceType;
+        }
+
+        public dynamic FindAll()
+        {
+            return db.InsuranceTypes.Select(i => new
+            {
+                i.Id,
+                i.Name,
+                i.Description
+                
+            }).ToList();
+        }
+
+        public dynamic FindById(int id)
+        {
+            return db.InsuranceTypes.Select(i => new
+            {
+                i.Id,
+                i.Name,
+                i.Description
+            }).SingleOrDefault(i => i.Id == id);
+        }
+
+        public dynamic Update(InsuranceType insuranceType)
+        {
+            db.InsuranceTypes.Add(insuranceType);
+            db.Entry(insuranceType).State = EntityState.Modified;
+            db.SaveChanges();
+            return insuranceType;
+        }
+    }
+}
