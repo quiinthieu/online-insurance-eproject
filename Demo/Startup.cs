7b27a8f8b82c214fba23d2b041492ecebfe5f52d
Billie Silvaner <quiinthieu@gmail.com>
2021-11-11 20:00:31 +0700

    Version 1.2.0 ✅

--- Demo/Startup.cs
+++ Demo/Startup.cs
@@ -60,7 +60,7 @@ namespace Demo
             app.UseExceptionHandler("/error");
             app.UsePathBase("/api");
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
